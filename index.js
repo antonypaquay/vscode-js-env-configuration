f69178cefa27bac4d145ef5752577b90b28f539e
Antony Paquay <antony.paquay@gmail.com>
2020-11-02 09:55:36 +0100

    [INIT] initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Edit and save this files to show changes
+const myArr = [1, 34, 98];
+
+console.log("test");
+
+function addition(a, b, c) {
+  const result = a + b + c;
+  console.log(result);
+}
+
+console.log(addition(myArr[0], myArr[1], myArr[3]));
